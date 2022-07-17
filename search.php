bbe55055defac3f1c592c7cde88f3b214a2d4732
pbol377 <39047597+pbol377@users.noreply.github.com>
2022-07-18 07:11:39 +0900

    Add files via upload

---
+++ search.php
@@ -0,0 +1,50 @@
+<meta charset="utf-8">
+<?php
+  echo "<h1>";
+  $search = $_GET['id'];
+  
+  $client_id = "";
+  $client_secret = "";
+  
+  $encText = urlencode($search);
+  
+  $url = "https://openapi.naver.com/v1/search/encyc.json?query=".$encText; // json 결과
+  $is_post = false;
+  
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_POST, $is_post);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  
+  $headers = array();
+  $headers[] = "X-Naver-Client-Id: ".$client_id;
+  $headers[] = "X-Naver-Client-Secret: ".$client_secret;
+  
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  
+  $response = curl_exec ($ch);
+  $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  curl_close ($ch);
+  if($status_code == 200) {
+     $file = json_decode($response);
+     $count = 1;
+     $count2 = 1;
+     foreach($file as $key => $val){
+        if ($key == "lastBuildDate") echo "검색일자:".$val."\n";
+           if($count2 == 1)echo "---------------------------------------------------------";
+              $count2 = 2;
+           if ($key == "items"){
+              foreach ($val as $key3 => $val3){
+              	$arr = get_object_vars($val3);
+                  echo $count.". ".$arr["title"]."<br><br>\n".$arr["description"]."<br><br><br>";
+                  echo "---------------------------------------------------------<br><br><br>";
+                  $count++;
+              }
+          }
+          echo "<br>";
+      }
+  } else {
+    echo "Error 내용:".$response;
+  }
+?>
---
+++ searchdict.php
@@ -0,0 +1,49 @@
+<meta charset="utf-8">
+<?php
+
+$search = $_GET['did'];
+
+$search= urlencode($search);
+
+//$key = "";
+
+$url = "https://stdict.korean.go.kr/api/search.do?key=&type_search=search&q=".$search;
+
+$ch = curl_init(); //curl 초기화
+curl_setopt($ch, CURLOPT_URL, $url); //URL 지정하기 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //요청 결과를 문자열로 반환 
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10); //connection timeout 10초 
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //원격 서버의 인증서가 유효한지 검사 안함
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method); 
+$response = curl_exec($ch); 
+$code = curl_getinfo($ch, CURLINFO_HTTP_CODE); 
+$error = curl_error($ch); 
+curl_close($ch);
+
+$xml = simplexml_load_string($response, "SimpleXMLElement", LIBXML_NOCDATA); 
+$json = json_encode($xml); 
+$array = json_decode($json, true);
+
+$count = 0;
+echo "<h1>";
+	if(is_array($array["item"])){
+		foreach ($array["item"] as $key => $val){
+			if($array["total"] != "1"){
+			   echo $val["sup_no"].". ".$val["word"]." [ 품사: ".$val["pos"]." ]<br><br>";
+			   echo $val["sense"]["definition"]."<br><br>";
+	           echo "<br><br>";
+	           echo "---------------------------------------------------------<br><br><br>";
+	           } else{
+	           if(is_array($array)){
+		          if($count == 1) break;
+		              echo $array["item"]["word"]." [ 품사: ".$array["item"]["pos"]." ]<br><br>";
+			          echo $array["item"]["sense"]["definition"]."<br><br><br><br>";
+	                  echo "<br><br>";
+	                  $count++;
+	                }
+		        }
+			}
+		} else{
+		      echo("존재하지 않는 단어입니다.");
+			}
+?>
